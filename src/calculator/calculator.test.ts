adb262e424a6b71914d64ce999350572da79f0e3
Thomas Hu <tomhu1096@gmail.com>
2020-06-24 18:29:22 -0400

    Stage 2

--- src/calculator/calculator.test.ts
+++ src/calculator/calculator.test.ts
@@ -4,10 +4,3 @@ test('adds 2 + 3 to equal 5', () => {
   const calc = new Calculator()
   expect(calc.add(2, 3)).toBe(5);
 });
-
-test('adds 2 + 6 to equal 8', () => {
-  const calc = new Calculator()
-  expect(false);
-
-  expect(calc.add(2, 3)).toBe(5);
-});
